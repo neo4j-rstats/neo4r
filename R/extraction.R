7df41e91780416b7a9668f85023e7ee99a9d17d7
Colin <colin@thinkr.fr>
2018-03-29 17:30:55 +0200

    extract functions

---
+++ R/extraction.R
@@ -0,0 +1,18 @@
+#' Extract nodes or relationships
+#'
+#' @param x a result from Neo4J
+#'
+#' @return a tibble
+#' @export
+#' @rdname extract
+
+extract_nodes <- function(x){
+  x$nodes
+}
+
+#' @export
+#' @rdname extract
+
+extract_relationships <- function(x){
+  x$relationships
+}
