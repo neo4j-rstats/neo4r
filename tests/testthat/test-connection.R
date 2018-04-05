c9ef2b513f9c504d398f7e99d0db56e51522e7ad
statnmap <sebastienrochettefr@gmail.com>
2018-04-05 15:11:47 +0200

    Add Travis badge and test

--- tests/testthat/test-connection.R
+++ tests/testthat/test-connection.R
@@ -1,11 +1,12 @@
 context("test-connection.R")
 
-library(neo4r)
-
-test_that("connection work", {
 library(httr)
-con <- neo4j_api$new(url = "http://localhost:7474", user = "neo4j", password = "neo4j")
-expect_null(con$get_version())
+
+
+test_that("connection work (on new install only)", {
+con <- neo4j_api$new(url = "http://localhost:7474",
+                     user = "neo4j", password = "neo4j")
+expect_is(con$get_version(), "character")
 })
 
 
