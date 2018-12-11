b461f7512664a81bcf599c88743678f1b235a2b1
Colin <colin@thinkr.fr>
2018-12-11 17:06:59 +0100

    init tests

--- tests/testthat/test-connection.R
+++ tests/testthat/test-connection.R
@@ -1,12 +1,9 @@
 context("test-connection.R")
 
-library(httr)
-
-
-test_that("connection work (on new install only)", {
-con <- neo4j_api$new(url = "http://localhost:7474",
-                     user = "neo4j", password = "neo4j")
-expect_is(con$get_version(), "character")
+test_that("connection work", {
+  expect_is(con, "Neo4JAPI")
+  expect_is(con, "R6")
+  expect_equal(con$ping(), "200")
 })
 
 
