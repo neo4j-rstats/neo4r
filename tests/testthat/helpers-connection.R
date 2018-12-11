6cf663e07ae9aa7cea85a26aa2f6be1ea0b8e6ed
Colin <colin@thinkr.fr>
2018-12-11 17:27:56 +0100

    Added some tests to the list

--- tests/testthat/helpers-connection.R
+++ tests/testthat/helpers-connection.R
@@ -1,3 +1,3 @@
 library(neo4r)
-con <- neo4j_api$new(url = "http://localhost:7474",  user = "neo4j", password = "neo4j")
+con <- neo4j_api$new(url = "http://localhost:7474",  user = "neo4j", password = "pouetpouet")
 
